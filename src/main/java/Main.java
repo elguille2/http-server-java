0ce999572f02893422d670cca737df6e50a14628
elguille2 <guillermo2pacheco@gmail.com>
2024-07-25 18:44:00 +0200

    Response with 200

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,46 @@
 import java.io.IOException;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class Main {
   public static void main(String[] args) {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
-
-    // Uncomment this block to pass the first stage
-    //
-     try {
-       ServerSocket serverSocket = new ServerSocket(4221);
-
-       // Since the tester restarts your program quite often, setting SO_REUSEADDR
-       // ensures that we don't run into 'Address already in use' errors
-       serverSocket.setReuseAddress(true);
-
-       serverSocket.accept(); // Wait for connection from client.
-       System.out.println("accepted new connection");
-     } catch (IOException e) {
-       System.out.println("IOException: " + e.getMessage());
-     }
-
-
+    new Main().startServer();
   }
+
+    private void startServer() {
+        // Try-with-resources to automatically close the server socket
+        try (ServerSocket serverSocket = new ServerSocket(4221)){
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
+
+            while(true){
+                Socket clientSocket = serverSocket.accept(); // Wait for connection from client
+                System.out.println("Accepted new connection");
+                handleClientConnection(clientSocket);
+            }
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
+        }
+    }
+
+    private void handleClientConnection(Socket clientSocket) {
+      // Try-with-resources to automatically close the output stream
+      try (OutputStream outputStream = clientSocket.getOutputStream()) {
+          String httpResponse = "HTTP/1.1 200 OK\r\n\r\n";
+          outputStream.write(httpResponse.getBytes("UTF-8"));
+          // Ensure all data is sent by flushing the output stream
+          outputStream.flush();
+      } catch (IOException e){
+          System.out.println("IOException while handling client connection: " + e.getMessage());
+      } finally {
+          try {
+              clientSocket.close();
+          } catch (IOException e){
+              System.out.println("IOException while closing client socket: " + e.getMessage());
+          }
+      }
+    }
 }
